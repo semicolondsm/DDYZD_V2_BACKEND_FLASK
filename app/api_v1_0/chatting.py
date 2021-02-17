2ab6b94f2b0da46c4e7ba3ec2629d0adc930b9a9
hwc9169 <hwc9169@gmail.com>
2021-02-17 20:48:25 +0900

    UPDATE; room_token

--- app/api_v1_0/chatting.py
+++ app/api_v1_0/chatting.py
@@ -1,11 +1,12 @@
-from . import api_v1_0 as api
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models import User, Room, ClubHead, Club
 from app import websocket
 from flask_socketio import join_room
 from flask_socketio import emit
 from app import logger
+from config import Config
 import json
+import jwt
 
 # 채팅들 리스트 반환
 @jwt_required()
@@ -18,16 +19,16 @@ def chat_list():
     return json.dumps(rooms, ensure_ascii=False).encode('utf8')
 
 
-# 채팅방 입장
+# 룸 토큰 반환
 @jwt_required()
-def enter_room(club_id):
+def room_token(club_id):
     club_head_id = ClubHead.query.filter_by(club_id=club_id).first().id
     room = Room.query.filter_by(user=get_jwt_identity(), club_head_id=club_head_id).first()
     if room is None:
         room = Room(user=get_jwt_identity(), club_head_id=club_head_id)
-    join_room(room.id)
+    token = jwt.encode({"room_id": room.id, 'sub': get_jwt_identity()}, Config.ROOM_SECRET_KEY, algorithm="HS256")
 
-    return {"room_id": room.id}, 200
+    return {'room-token': token}, 200
     
 
 @api.route('/club/<int:room_id>/chat', methods=['POST'])
