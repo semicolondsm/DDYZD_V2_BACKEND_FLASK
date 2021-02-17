433d991d74303c1213ee5a0718d8c1a99eae124b
hwc9169 <hwc9169@gmail.com>
2021-02-17 20:59:36 +0900

    UPDATE; test room token api

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 from app import create_app
+from app import websocket
 from app import db
 import jwt
 import os
 import pytest
 load_dotenv()
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def flask_app():
     app = create_app('test')
     app_context = app.app_context()
@@ -19,12 +20,18 @@ def flask_app():
     db.drop_all()
     app_context.pop()
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def flask_client(flask_app):
     return flask_app.test_client()
 
-@pytest.fixture
+@pytest.fixture(scope='session')
+def flask_websocket(flask_app, flask_client):
+    flask_websocket = websocket.test_client(flask_app, flask_test_client=flask_client)
+    flask_websocket.connect('/chat')
+    return flask_websocket
+
+@pytest.fixture(scope='session')
 def headers():
     token = jwt.encode({"sub": 1}, os.getenv("SECRET"), algorithm="HS256")
-    headers = {'Authorization': 'Bearer '+ token.decode('utf-8')}
+    headers = {'Authorization': 'Bearer '+ token}
     return headers
--- test/test_chatting_api.py
+++ test/test_chatting_api.py
@@ -1,24 +1,30 @@
 import pytest
 import json
+import jwt
+from config import Config
 from app import db
 from app.models import Room, Chat, Club, ClubHead, User, Application
 
-def test_ping(flask_client):
-    resp = flask_client.get("ping")
-    assert resp.status_code ==  200
-    assert resp.json.get('msg') == 'ping successfully'
-
-
-def test_chat_list(flask_client, headers):
+@pytest.fixture(scope='module')
+def db_setting():
     db.session.add(User(name='김수완', gcn='1103'))
     db.session.add(User(name='조호원', gcn='1118'))
     db.session.add(Club(club_name='세미콜론'))
     db.session.add(ClubHead(user_id=2, club_id=1))
     db.session.add(Room(user=1, club_head_id=1))
     db.session.add(Chat(room_id=1, msg='첫번째 채팅', user_type='U'))
+
     db.session.commit()
 
-    resp = flask_client.get("/chat/list", headers=headers)
+
+def test_ping(flask_websocket):
+    resp = flask_websocket.flask_test_client.get("ping")
+    assert resp.status_code ==  200
+    assert resp.json.get('msg') == 'ping successfully'
+
+
+def test_chat_list(flask_websocket, headers, db_setting):
+    resp = flask_websocket.flask_test_client.get("/chat/list", headers=headers)
     data = resp.data.decode('utf8').replace("'", '"')
     data = json.loads(data)
 
@@ -30,5 +36,11 @@ def test_chat_list(flask_client, headers):
     assert data[0].get('clubname') == '세미콜론'
 
 
-def test_make_room():
-    pass
+def test_room_token(flask_websocket, headers, db_setting):  
+    resp = flask_websocket.flask_test_client.post("/chat/1/token", headers=headers)
+    token = resp.json['room-token']
+    payload = jwt.decode(token, Config.ROOM_SECRET_KEY, algorithms=["HS256"])
+
+    assert resp.status_code == 200
+    assert payload['room_id'] == 1
+    assert payload['sub'] == 1
