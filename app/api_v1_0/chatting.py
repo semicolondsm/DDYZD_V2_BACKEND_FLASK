183a69d2cc41cecc443b840f787a0b4eaa7a05ac
hwc9169 <hwc9169@gmail.com>
2021-02-20 23:42:28 +0900

    UPDATE; send chat event, room token api

--- app/api_v1_0/chatting.py
+++ app/api_v1_0/chatting.py
@@ -1,22 +1,31 @@
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.errors.http import Forbidden
+from app.decorator import clubhead_required
+from app.decorator import room_token_required
 from app.models import User, Room, ClubHead, Club, Chat
+from app import db
+from app import logger
+from config import Config
+from datetime import datetime
+from datetime import timedelta
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 from flask_socketio import leave_room
 from flask_socketio import join_room
 from flask_socketio import emit
-from app import logger
-from config import Config
 from flask import request
-from datetime import datetime
-from datetime import timedelta
 import json
 import jwt
 
 
 # 채팅들 리스트 반환
 @jwt_required()
+@clubhead_required
 def chat_list():
-    rs = User.query.get_or_404(get_jwt_identity()).rooms
+    club_id = request.args.get('club_id')
+    user = User.query.get_or_404(get_jwt_identity())
     rooms = []
+    rs = user.rooms if club_id == None else Room.query.filter_by(club_id=club_id).all()
+
     for r in rs:
         rooms.append(r.json())
 
@@ -26,12 +35,12 @@ def chat_list():
 # 채팅방 만들기
 @jwt_required()
 def make_room(club_id):
-    room = Room.query.filter_by(user_id=get_jwt_identity(), club_id=club_id).first_or_404()
+    room = Room.query.filter_by(user_id=get_jwt_identity(), club_id=club_id).first()
     if room is None:
-        room = Room(user=get_jwt_identity(), club_head_id=club_head_id, user_looked=False, head_looked=False)
+        room = Room(user_id=get_jwt_identity(), club_id=club_id)
         db.session.add(room)
         db.session.commit()
-    
+
     return {'room_id': room.id}, 200
 
 
@@ -54,6 +63,22 @@ def chat_section():
 
     return json.dumps(sections, ensure_ascii=False).encode('utf8'), 200
 
+
+# 룸 토큰 반환
+@jwt_required()
+def room_token(room_id):
+    room = Room.query.get_or_404(room_id)
+    user = User.query.get_or_404(get_jwt_identity()) 
+    if user.is_user(room=room):
+        token = jwt.encode({"room_id": room.id, 'user_id': get_jwt_identity(), "user_type": 'U', \
+            "exp": datetime.utcnow()+timedelta(days=1)}, Config.ROOM_SECRET_KEY, algorithm="HS256")
+    elif user.is_clubhead(room=room):
+        token = jwt.encode({"room_id": room.id, 'user_id': get_jwt_identity(), "user_type": 'C', \
+            "exp": datetime.utcnow()+timedelta(days=1)}, Config.ROOM_SECRET_KEY, algorithm="HS256")
+
+    return {'room_token': token}, 200
+
+
 # 소켓 연결
 @jwt_required()
 def connect():
@@ -62,24 +87,27 @@ def connect():
 
 
 # 방 입장
+@room_token_required
 def event_join_room(json):
     join_room(json.get('room_id'))
     emit('response', {'msg': 'Join Room Success'}, namespace='/chat')
 
 
+# 채팅 보내기
+@room_token_required
+def event_send_chat(json):
+    logger.info('JSON: '+str(json))
+    emit('recv_chat', {'data': json.get('data')}, room=json.get('room_id'))
+    db.session.add(Chat(room_id=json.get('room_id'), msg=json.get('data'), user_type=json.get('user_type')))
+    db.session.commit()    
+
 # 방 나가기
+@room_token_required
 def event_leave_room(json):
     leave_room(json.get('room_id'))
     emit('response', {'msg': 'Leave Room Success'}, namespace='/chat')
 
 
-# 채팅 보내기
-def event_send_chat(json):
-    emit('recv_chat', {'data': json.get('data')}, room=json.get('room_id'))
-    logger.info(str(json))
-    
-
 # 소켓 연결 끊기
 def disconnect():
     logger.info('[Socket Disconnected]')
-
