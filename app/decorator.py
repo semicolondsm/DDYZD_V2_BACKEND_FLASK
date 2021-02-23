7181158fdc81acb8926f4e92c36f1e782e91713b
hwc9169 <hwc9169@gmail.com>
2021-02-23 17:42:27 +0900

    UPDATE; remove clubhead_required

--- app/decorator.py
+++ app/decorator.py
@@ -13,19 +13,6 @@ from flask import request
 import jwt
 
 
-def clubhead_required(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        club_id = request.args.get('club_id')
-        if club_id is not None:
-            club_head = ClubHead.query.filter_by(user_id=get_jwt_identity(), club_id=club_id).first()
-            if club_head is None:
-                return http.Forbidden()
-
-        return fn()
-    return wrapper
-
-
 def room_token_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
