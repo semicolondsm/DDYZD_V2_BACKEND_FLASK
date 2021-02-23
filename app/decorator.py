b5b6a32547c682f7bbb6bf22adb264fe54d119b4
hwc9169 <hwc9169@gmail.com>
2021-02-24 02:28:17 +0900

    UPDATE; clean code

--- app/decorator.py
+++ app/decorator.py
@@ -16,16 +16,15 @@ import jwt
 def room_token_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        logger.info(str(args))
         token = args[0].get('room_token')
         try:
             json = jwt.decode(token, Config.ROOM_SECRET_KEY, algorithms="HS256")
         except jwt.ExpiredSignatureError as e:
-            return emit('error', websocket.Unauthorized('ExpiredSignatureError'))
+            return emit('error', websocket.Unauthorized('ExpiredSignatureError'), namespace='/chat')
         except Exception as e:
-            return emit('error', websocket.Forbidden())
+            return emit('error', websocket.Forbidden(), namespace='/chat')
         json['args'] = args[0]
-
+        
         return fn(json)
     return wrapper
 
@@ -35,7 +34,7 @@ def apply_message_required(fn):
     def wrapper(json):
         json['major'] = json.get('args').get('major')
         if json['major'] is None:
-            return emit('error', {'msg': websocket.BadRequest('Please send with major')}, namespace='/chat')
+            return emit('error', websocket.BadRequest('Please send with major'), namespace='/chat')
 
         return fn(json)
     return wrapper
@@ -46,8 +45,8 @@ def chat_message_required(fn):
     def wrapper(json):
         json['msg'] = json.get('args').get('msg')
         if json['msg'] is None:
-            return emit('error', {'msg': websocket.BadRequest('Please send with message')}, namespace='/chat')
-
+            return emit('error', websocket.BadRequest('Please send with message'), namespace='/chat')
+        
         return fn(json)
     return wrapper
 
@@ -55,6 +54,10 @@ def chat_message_required(fn):
 def schedule_information_required(fn):
     @wraps(fn)
     def wrapper(json):
-
+        json['date'] = json.get('args').get('date')
+        json['location'] = json.get('args').get('location')
+        if json['date'] is None or json['location'] is None:
+            return emit('error', websocket.BadRequest('Please send with date and location'), namespace='/chat')
+            
         return fn(json)
     return wrapper
