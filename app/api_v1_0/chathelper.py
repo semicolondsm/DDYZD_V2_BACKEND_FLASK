516f5fce3900f88a79ac2379c261bb57a0976d0d
hwc9169 <hwc9169@gmail.com>
2021-02-23 18:27:01 +0900

    UPDATE; helper_apply event

--- app/api_v1_0/chathelper.py
+++ app/api_v1_0/chathelper.py
@@ -1,22 +1,23 @@
 from app.decorator import room_token_required
-from app.models import User, Club, Major
+from app.models import User, Club, Major, Application
 from app.errors import websocket
 from app import logger
+from app import db
 from datetime import datetime
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_socketio import emit
 
 
-def get_apply_message(user, club, field=None):
+def get_apply_message(user, club, major):
     title= '{name}님이 지원하셨습니다.'.format(name=user.name) 
-    msg = '{gcn} {name}님이 {club}에 {field}분야로 지원하셨습니다.'\
-        .format(gcn=user.gcn, name=user.name, club=club.club_name, field=field)
+    msg = '{gcn} {name}님이 {club}에 {major}분야로 지원하셨습니다.'\
+        .format(gcn=user.gcn, name=user.name, club=club.club_name, major=major.major_name)
     
     return {'title': title, 'msg': msg}
 
 
-# 채팅들 리스트 반환
+# 동아리 지원
 @room_token_required
 def helper_apply(json):
     if json.get('user_type') != 'U':
@@ -24,14 +25,25 @@ def helper_apply(json):
     user = User.query.get(json.get('user_id'))
     club = Club.query.get(json.get('club_id'))
     major = Major.query.filter_by(club_id=json.get('club_id'), major_name=json.get('major')).first()
-
+    
+    # 동아리에 이미 가입한 경우
+    if user.is_member(club):
+        return emit('error', {'msg': websocket.BadRequest('You are already member of this club')}, namespace='/chat')
+    # 동아리에 이미 신청한 경우
+    if user.is_applicant(club=club, result=False):
+        return emit('error', {'msg': websocket.BadRequest('You are already apply to this club')}, namespace='/chat')
+    # 동아리 지원 기간이 아닌 경우
     if not club.is_recruiting():
         return emit('error', {'msg': websocket.BadRequest('Club is not recruiting now!')}, namespace='/chat')
-    if major not in club.majors:
-        return emit('error', {'msg': websocket.BadRequest('Club does not need '+major.major_name)}, namespace='/chat')
+    # 동아리가 모집하는 분야가 아닐 때 경우
+    if major is None:
+        return emit('error', {'msg': websocket.BadRequest('Club does not need '+str(json.get('major')))}, namespace='/chat')
     
-    emit('response', get_apply_message(user, club, json.get('field')), room=json.get('room_id'))
-    logger.info(json.get('room_id'))
+    db.session.add(Application(club_id=json.get('club_id'), user_id=json.get('user_id'), result=False))
+    db.session.commit()
+    
+    logger.info('[Helper Apply]'+str(get_apply_message(user=user, club=club, major=major)))
+    emit('response', get_apply_message(user=user, club=club, major=major), room=json.get('room_id'))
 
 
 def helper_schedule(json):
