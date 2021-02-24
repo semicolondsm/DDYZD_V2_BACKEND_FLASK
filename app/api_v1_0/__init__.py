efc583bb470c7d19d1d2723975cd738441277e4f
hwc9169 <hwc9169@gmail.com>
2021-02-24 20:27:33 +0900

    UPDATE; add refresh room route

--- app/api_v1_0/__init__.py
+++ app/api_v1_0/__init__.py
@@ -18,6 +18,7 @@ api_v1_0.add_url_rule('/chat/<int:room_id>/breakdown', 'breakdown', breakdown, m
 api_v1_0.add_url_rule('/room/<int:room_id>/token', 'room_token', room_token, methods=['GET'])
 api_v1_0.add_url_rule('/club/<int:club_id>/applicant', 'applicant_list', applicant_list, methods=['GET'])
 api_v1_0.add_url_rule('/room/<int:room_id>/info', 'room_info', room_info, methods=['GET'])
+api_v1_0.add_url_rule('/room/<int:room_id>/refresh', 'room_refresh', room_refresh, methods=['GET'])
 
 # chathelper event
 flask_websocket.on_event('helper_apply', helper_apply, namespace='/chat')
