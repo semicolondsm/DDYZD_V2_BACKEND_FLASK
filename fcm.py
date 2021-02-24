7ea5202d2e3bde93ff18cd8dee05541f5e5e76e4
hwc9169 <hwc9169@gmail.com>
2021-02-24 20:28:50 +0900

    UPDATE; test room refresh

--- fcm.py
+++ fcm.py
@@ -9,19 +9,19 @@ default_app = firebase_admin.initialize_app(cred)
 # This registration token comes from the cli
 # ent FCM SDKs.
 #registration_token = 'ANDROID_CLIENT_TOKEN'
-token = 'fLfyEDFZAE4HuSRIIB7FuK:APA91bFdfN5CXf0Wsyjs8-vGeA0pBXo-7mGpC49BXu124s6ZIAluEC3i-pMZYEC_OtUzmZshfGyWRIEpzidHOXf5tPuIXJ-e59cvNbPdHHZ8oY4tKpEePkbarsIXgja5q1FLwVOyna85'
+token = 'dhaLr2D6QkCqzQcljYgFfk:APA91bGCRFToQ0gPlrnumzcpQWzeE05-BI6Jk-aOvbkWCl9RluPUdhmA9zN996W15D1M3qwipDK-ocNH3xwxA1ti0bhht9Oh5s_E1DJnkkJdMu7fOhdaJM1YduR0-HoV4KIcO-_LDnyU'
 # See documentation on defining a message payload.
-aps = messaging.Aps(messaging.ApsAlert(title="APs Alert Title", subtitle="APs Alert Subtitle", body="APs Alert Body"))
+aps = messaging.Aps(messaging.ApsAlert(title="APs Alert Title", subtitle="APs Alert Subtitle", body="APs Alert Body"),sound="default")
 aps = messaging.APNSPayload(aps, custom='data')
 message = messaging.Message(
     notification=messaging.Notification(
-        title='넌',
-        body='밤새 알림보낸다',
+        title='우리 수완이',
+        body='화이팅',  
         ),
     # apns=messaging.APNSConfig(payload=aps),
     token=token
 )
-for i  in range(1):
+for i  in range(50):
     response = messaging.send(message)
 
 # Send a message to the device corresponding to the provided
--- test/test_chatting.py
+++ test/test_chatting.py
@@ -123,17 +123,9 @@ def test_applicant_list(flask_client, db_setting):
     assert data[0]['index'] == 0
     
 
-## 채팅방 정보 반환 ##
+## 채팅방 정보 반환 테스트##
 def test_room_info(flask_client, db_setting):
     resp = flask_client.get('/room/1/info', headers=jwt_token(1))
-    assert resp.status_code == 200
-    data = resp.json
-
-    assert data['id'] == '1'
-    assert data['name'] == '세미콜론'
-    assert data['image'] == 'https://api.semicolon.live/file/profile_image'
-    
-    resp = flask_client.get('/room/1/info', headers=jwt_token(2))
     assert resp.status_code == 200
     data = resp.json
 
@@ -141,6 +133,38 @@ def test_room_info(flask_client, db_setting):
     assert data['name'] == '조호원'
     assert data['image'] == 'profile2'
     
+    resp = flask_client.get('/room/1/info', headers=jwt_token(2))
+    assert resp.status_code == 200
+    data = resp.json
+    
+    assert data['id'] == '1'
+    assert data['name'] == '세미콜론'
+    assert data['image'] == 'https://api.semicolon.live/file/profile_image'
+
+
+## 채팅방 리프레시 테스트 ##
+def test_room_refresh(flask_client, db_setting):
+    resp = flask_client.get('/room/1/refresh', headers=jwt_token())
+    assert resp.status_code == 200
+    data = resp.json
+
+    assert data.get('id') == '2' 
+    assert data.get('name') == '조호원' 
+    assert data.get('image') == 'profile2' 
+    assert data.get('lastmessage') == '두번째 채팅' 
+    assert data.get('lastdate') != None 
+    assert data.get('index') == 0
+    
+    resp = flask_client.get('/room/1/refresh', headers=jwt_token(sub=2))
+    assert resp.status_code == 200
+    data = resp.json
+
+    assert data.get('id') == '1' 
+    assert data.get('name') == '세미콜론' 
+    assert data.get('image') == 'https://api.semicolon.live/file/profile_image' 
+    assert data.get('lastmessage') == '두번째 채팅' 
+    assert data.get('lastdate') != None 
+    assert data.get('index') == 0
 
 
 ## 방 들어가기 테스트 (채팅 보내기 전에 먼저 실행하자)## 
