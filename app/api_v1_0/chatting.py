dd7564d84410a27fee64d9199c351930e55c50d7
hwc9169 <hwc9169@gmail.com>
2021-02-24 16:42:13 +0900

    UPDATE; chat list add parameter

--- app/api_v1_0/chatting.py
+++ app/api_v1_0/chatting.py
@@ -22,24 +22,34 @@ import jwt
 @jwt_required()
 def chat_list():
     user = User.query.get_or_404(get_jwt_identity())
-    index = 0
+    club_id = request.args.get('club_id')
     rooms = []
-    # 채팅 섹션
-    club_section = [user.name] 
-    for club in user.get_clubs():
-        club_section.append(club.club_name)
-
-    # 유저 권한의 채팅방 검색
-    for r in user.rooms.all():    
-        rooms.append(r.json(is_user=True, index=index))
-
-    # 동아리장 권한의 채팅방 검색
-    for c in user.get_clubs():
-        index = index + 1
-        for r in c.rooms:
+    index = 0
+    # club_id 파라미터가 있을 때 
+    if club_id:
+        club = Club.query.get_or_404(club_id)
+        if not user.is_clubhead(club):
+            return http.BadRequest('You do not have permission')
+        for r in club.rooms:
             rooms.append(r.json(is_user=False, index=index))
+        return {"club_section": club.club_name, "rooms": rooms}, 200
+    else:
+        # 채팅 섹션
+        club_section = [user.name] 
+        for club in user.get_clubs():
+            club_section.append(club.club_name)
 
-    return {"club_section": club_section, "rooms": rooms}, 200
+        # 유저 권한의 채팅방 검색
+        for r in user.rooms.all():    
+            rooms.append(r.json(is_user=True, index=index))
+
+        # 동아리장 권한의 채팅방 검색
+        for c in user.get_clubs():
+            index = index + 1
+            for r in c.rooms:
+                rooms.append(r.json(is_user=False, index=index))
+
+        return {"club_section": club_section, "rooms": rooms}, 200
 
 
 # 채팅방 만들기
