01a01fc040d6b84a759b89cc321d8e8894466c79
hwc9169 <hwc9169@gmail.com>
2021-02-26 01:03:46 +0900

    UPDATE; test send alarm decorator

--- test/test_chatting.py
+++ test/test_chatting.py
@@ -8,7 +8,7 @@ import jwt
 import pytest
 
 ## 테스트 핑 ## 
-def test_ping(flask_websocket):
+def test_ping(db_setting, flask_websocket):
     resp = flask_websocket.flask_test_client.get("ping")
 
     assert resp.status_code ==  200
@@ -184,16 +184,17 @@ def test_send_chat(flask_websocket, db_setting):
     flask_websocket.emit('send_chat',{'msg': 'Hello \U0001f600', 'room_token': room_token()}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[0]
 
-    assert recv['name'] != 'error'
+    assert recv['name'] == 'recv_chat'
     assert recv['args'][0]['msg'] == 'Hello \U0001f600'
     assert recv['args'][0]['user_type'] == 'C'
     assert recv['args'][0]['date'] != None
+
     # 동아리원 채팅
     flask_websocket.emit('send_chat', {'msg': 'World!', 'room_token': room_token(user_id=2, user_type='U')}, namespace='/chat')
     recv = flask_websocket.get_received(namespace='/chat')[0]
 
-    assert recv['name'] != 'error'
-    assert recv['args'][0]['user_type'] == 'U'
+    assert recv['name'] == 'alarm'
+    assert recv['args'][0]['room_id'] == '1'
 
 
 ## 방 나가기 테스트 (채팅 보내고 난 뒤에 실행 되어야함!) ## 
