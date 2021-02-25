dffcd1e7937a9652b2a0e107aea6edd2ee82288c
hwc9169 <hwc9169@gmail.com>
2021-02-26 00:35:11 +0900

    UPDAET; send alarm

--- app/decorator.py
+++ app/decorator.py
@@ -15,6 +15,7 @@ import jwt
 
 
 def room_token_required(fn):
+
     @wraps(fn)
     def wrapper(*args, **kwargs):
         token = args[0].get('room_token')
@@ -28,6 +29,65 @@ def room_token_required(fn):
         
         return fn(json)
     return wrapper
+
+
+
+
+
+def room_member_required(fn):
+    @wraps(fn)
+    def wrapper(room_id):
+        room = Room.query.get_or_404(room_id)
+        user = User.query.get_or_404(get_jwt_identity())
+        if not user.is_member(room=room):
+            return http.BadRequest("You are not a member for the room: "+str(room.id))
+        
+        return fn(user, room)
+    return wrapper
+
+
+def club_member_required(fn):
+    @wraps(fn)
+    def wrapper(club_id):
+        club = Club.query.get_or_404(club_id)
+        user = User.query.get_or_404(get_jwt_identity())
+        if not user.is_member(club=club):
+            return http.BadRequest("You are not a member for the room: "+str(room.id))
+        
+        return fn(user, club)
+    return wrapper   
+
+
+def schedule_information_required(fn):
+    @wraps(fn)
+    def wrapper(json):
+        json['date'] = json.get('args').get('date')
+        json['location'] = json.get('args').get('location')
+        if json['date'] is None or json['location'] is None:
+            return emit('error', websocket.BadRequest('Please send with date and location'), namespace='/chat')
+            
+        return fn(json)
+    return wrapper
+
+
+def send_alarm(fn):
+    @wraps(fn)
+    def wrapper(json):
+        room = Room.query.get(json.get('room_id'))
+        if json.get('user_type') == 'U':
+            '''
+            일반 유저가 메시지를 보낸 경우
+            동아리장에게 알림이 간다
+            '''
+            user = room.club.club_head[0].club_head_user
+        else:
+            '''
+            동아리장이 메시지를 보낸 경우
+            일반 유저에게 알림이 간다
+            '''
+            user = room.user
+
+        emit('alarm', {'room_id': str(json.get('room_id'))}, room=json.get('sid'))
 
 
 def room_read(fn):
@@ -70,39 +130,3 @@ def chat_message_required(fn):
         
         return fn(json)
     return wrapper
-
-
-def schedule_information_required(fn):
-    @wraps(fn)
-    def wrapper(json):
-        json['date'] = json.get('args').get('date')
-        json['location'] = json.get('args').get('location')
-        if json['date'] is None or json['location'] is None:
-            return emit('error', websocket.BadRequest('Please send with date and location'), namespace='/chat')
-            
-        return fn(json)
-    return wrapper
-
-
-def room_member_required(fn):
-    @wraps(fn)
-    def wrapper(room_id):
-        room = Room.query.get_or_404(room_id)
-        user = User.query.get_or_404(get_jwt_identity())
-        if not user.is_member(room=room):
-            return http.BadRequest("You are not a member for the room: "+str(room.id))
-        
-        return fn(user, room)
-    return wrapper
-
-
-def club_member_required(fn):
-    @wraps(fn)
-    def wrapper(club_id):
-        club = Club.query.get_or_404(club_id)
-        user = User.query.get_or_404(get_jwt_identity())
-        if not user.is_member(club=club):
-            return http.BadRequest("You are not a member for the room: "+str(room.id))
-        
-        return fn(user, club)
-    return wrapper   
