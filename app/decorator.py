8b0ce012969beaec312bbf93999caf981da50dd9
hwc9169 <hwc9169@gmail.com>
2021-02-26 00:52:08 +0900

    UPDATE; decorator description

--- app/decorator.py
+++ app/decorator.py
@@ -15,7 +15,11 @@ import jwt
 
 
 def room_token_required(fn):
-
+    '''
+    요약: 채팅방 토큰을 요구하는 데코레이터
+    send_chat, join_room, leave_room,
+    helper_apply, helper_schedule, helper_result에 사용한다.
+    '''
     @wraps(fn)
     def wrapper(*args, **kwargs):
         token = args[0].get('room_token')
@@ -31,10 +35,13 @@ def room_token_required(fn):
     return wrapper
 
 
-
-
-
 def room_member_required(fn):
+    '''
+    요약: 채팅방 맴버인지 확인하는 데코레이터
+    room_info, breakdown, room_token, room_refresh에 사용한다.
+    rest api에서 사용되는 것은 room_member_required 이고,
+    websocket event를 비슷하게 처리하기위해 room_token_required를 사용해야한다.
+    '''
     @wraps(fn)
     def wrapper(room_id):
         room = Room.query.get_or_404(room_id)
@@ -47,6 +54,10 @@ def room_member_required(fn):
 
 
 def club_member_required(fn):
+    '''
+    요약: 동아리 맴버인지 확인하는 데코레이터 
+    applicant_list에서 사용한다.
+    '''
     @wraps(fn)
     def wrapper(club_id):
         club = Club.query.get_or_404(club_id)
@@ -59,6 +70,10 @@ def club_member_required(fn):
 
 
 def schedule_information_required(fn):
+    '''
+    요약: helper_schedule 이벤트에서 일정 정보를 처리하기 위한 데코레이터
+    room_token_required과 같이 연계되어 사용되어야한다.
+    '''
     @wraps(fn)
     def wrapper(json):
         json['date'] = json.get('args').get('date')
@@ -71,6 +86,10 @@ def schedule_information_required(fn):
 
 
 def send_alarm(fn):
+    '''
+    요약: 알람 보내는 처리를 하는 데코레이터
+    send_chat, helper_apply, helper_schedule, helper_result에서 사용한다.
+    '''
     @wraps(fn)
     def wrapper(json):
         room = Room.query.get(json.get('room_id'))
@@ -91,6 +110,10 @@ def send_alarm(fn):
 
 
 def room_read(fn):
+    '''
+    요약: 채팅방 읽음 처리 데코레이터
+    join_room, leave_room 이벤트에서 사용한다. 
+    '''
     @wraps(fn)
     def wrapper(json):
         room = Room.query.get(json.get('room_id'))
@@ -101,6 +124,12 @@ def room_read(fn):
 
 
 def room_writed(fn):
+    '''
+    요약: 채팅방 읽지 않음 처리 데코레이터
+    send_chat, helper_apply, helper_schedule, helper_result에서 사용한다.
+    일반 유저가 보낸경우 동아리장이 읽지 않음처리가 되고 동아리장이 보낸 경우엔 그 반대.
+    room_read 데코레이터와 처리 방법이 반대다.(헷갈릴 수 있음)
+    '''
     @wraps(fn)
     def wrapper(json):
         room = Room.query.get(json.get('room_id'))
@@ -111,6 +140,11 @@ def room_writed(fn):
 
 
 def apply_message_required(fn):
+    '''
+    요약: 동아리 지원 메시지 처리 데코레이터
+    동아리 지원시 메시지를 처리해주는 데코레이터다(에러 처리 및 전처리 포함)
+    helper_apply에서 사용한다.
+    '''
     @wraps(fn)
     def wrapper(json):
         json['major'] = json.get('args').get('major')
@@ -121,7 +155,12 @@ def apply_message_required(fn):
     return wrapper
 
 
-def chat_message_required(fn):
+def chat_message_required(fn):    
+    '''
+    요약: 채팅 메시지 처리 데코레이터
+    채팅시 메시지를 처리해주는 데코레이터다(에러 처리 및 전처리 포함)
+    send_chat에서 사용한다.
+    '''
     @wraps(fn)
     def wrapper(json):
         json['msg'] = json.get('args').get('msg')
