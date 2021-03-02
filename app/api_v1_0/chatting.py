4da822cf009c9f9dc7254e2814f71973089dae73
hwc9169 <hwc9169@gmail.com>
2021-03-02 22:21:21 +0900

    UPDATE; return status room info

--- app/api_v1_0/chatting.py
+++ app/api_v1_0/chatting.py
@@ -86,9 +86,9 @@ def make_room(club_id):
 @room_member_required
 def room_info(user, room):
     if user.is_user(room):
-        return {'id': str(room.club.id), 'name': room.club.name,'image': 'https://api.semicolon.live/file/'+room.club.profile_image}
+        return {'id': str(room.club.id), 'name': room.club.name,'image': 'https://api.semicolon.live/file/'+room.club.profile_image, 'status': room.status.name}
     else:
-        return {'id': str(room.user.gcn), 'name': room.user.name, 'image': room.user.image_path}
+        return {'id': str(room.user.gcn), 'name': room.user.name, 'image': room.user.image_path, 'status': room.status.name}
 
 
 # 채팅 내역 보기
