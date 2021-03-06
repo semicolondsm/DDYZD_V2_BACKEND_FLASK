9dece1ac96d8c2cc6658e7ed33a885cd258a7dd9
hwc9169 <hwc9169@gmail.com>
2021-03-06 10:00:07 +0900

    Revert "UPDATE; room info"
    This reverts commit 3e9a5a10aaec4cce57cab7d9e987cde3a3c93624.

--- app/api_v1_0/chatting.py
+++ app/api_v1_0/chatting.py
@@ -93,7 +93,7 @@ def room_info(user, room):
     if user.is_user(room):
         return {'id': str(room.club.id), 'name': room.club.name,'image': 'https://api.semicolon.live/file/'+room.club.profile_image, 'status': room.status.name}
     else:
-        return {'id': str(room.user.gcn), 'name': str(room.user.gcn)+room.user.name, 'image': room.user.image_path, 'status': room.status.name}
+        return {'id': str(room.user.gcn), 'name': room.user.name, 'image': room.user.image_path, 'status': room.status.name}
 
 
 # 채팅 내역 보기
