697b375a598d62c1ba5cff80cfb85a67289f964e
hwc9169 <hwc9169@gmail.com>
2021-03-06 10:36:09 +0900

    UPDATE; handshake jwt required

--- app/decorator.py
+++ app/decorator.py
@@ -18,6 +18,33 @@ import asyncio
 import jwt
 
 
+def handshake_jwt_required(fn):
+    '''
+    요약: 핸드쉐이크시 jwt 토큰 인증하는 데코레이터
+    Web의 경우 Authorization 헤더를 보낼 수 없기 때문에 url query로 토큰을 보낸다.
+    하지만 앱(안드로이드, iOS)의 경우에는 헤더에서 토큰을 받는다.
+    '''
+    @wraps(fn)
+    def wrapper():
+        from app import logger
+        logger.info("ARGS: "+str(request.args.get('token')))
+        logger.info("HEADERS: "+str(request.headers.get('Authorization')))
+        if request.args.get('token'):
+            token = request.args.get('token') 
+        else:
+            token = request.args.get('Authorization')
+        try:
+            payload = jwt.decode(token, Config.JWT_SECRET_KEY, algorithms="HS256")
+        except jwt.ExpiredSignatureError:
+            return http.Unauthorized("ExpiredSignatureError")
+        except Exception as e:
+                return emit('error', websocket.Unauthorized(e), namespace='/chat')
+        logger.info("PAYLOAD: "+str(payload))
+        user = User.query.get_or_404(payload.get('sub'))
+
+        return fn(user)
+    return wrapper
+    
 
 def room_token_required(fn):
     '''
