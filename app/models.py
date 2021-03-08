7d196aa5fd7e95164430fe99c4587d6c727c125e
hwc9169 <hwc9169@gmail.com>
2021-03-08 13:25:54 +0900

    UPDATE; contents varchar 4001

--- app/models.py
+++ app/models.py
@@ -299,7 +299,7 @@ class Feed(db.Model):
     __tablename__ = 'feed'
     id = db.Column(db.Integer, primary_key=True)
     club_id = db.Column(db.Integer, db.ForeignKey("club.id"))
-    contents = db.Column(db.String(255))
+    contents = db.Column(db.String(4001))
     upload_at = db.Column(db.DateTime())
     pin = db.Column(db.Boolean(), default=False)
 
