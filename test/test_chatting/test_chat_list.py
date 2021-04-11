c36111d63391a0e4c03936b88f2948cc26375a2c
hwc9169 <hwc9169@gmail.com>
2021-04-11 15:05:57 +0900

    UPDATE; test chat list after deleting room

--- test/test_chatting/test_chat_list.py
+++ test/test_chatting/test_chat_list.py
@@ -1,4 +1,7 @@
 from test.conftest import jwt_token
+from app.models.chat import Room
+from app.models.user import User
+from app.models.type import UserType
 import json
 
 ## 채팅 리스트 불러오기 테스트 ## 
@@ -33,3 +36,39 @@ def test_chat_list(flask_client, db_setting):
     assert data['rooms'][0].get('lastdate') != None
     assert data['rooms'][0].get('isread') == False
     assert data['rooms'][0].get('index') == 0
+
+    # 채팅방 삭제시
+    room = Room.query.get(1)
+    room.delete_chats(UserType.C.name)
+
+    resp = flask_client.get("/chat/list", headers=jwt_token(1))
+    assert resp.status_code == 200
+    data = resp.data.decode('utf8').replace("'", '"')
+    data = json.loads(data)
+    assert data['club_section'] == ['김수완', '세미콜론']
+    assert data['rooms'] == []
+
+    resp = flask_client.get("/chat/list", headers=jwt_token(2))
+    assert resp.status_code == 200
+    data = resp.data.decode('utf8').replace("'", '"')
+    data = json.loads(data)
+
+    assert data['club_section'] == ['조호원']
+    assert data['rooms'][0].get('roomid') == '1'
+    assert data['rooms'][0].get('id') == '1'
+    assert data['rooms'][0].get('name') == '세미콜론'
+    assert data['rooms'][0].get('image') == 'https://api.semicolon.live/file/profile_image'
+    assert data['rooms'][0].get('lastmessage') == '두번째 채팅'
+    assert data['rooms'][0].get('lastdate') != None
+    assert data['rooms'][0].get('isread') == False
+    assert data['rooms'][0].get('index') == 0
+
+    room = Room.query.get(1)
+    room.delete_chats(UserType.U.name)
+    resp = flask_client.get("/chat/list", headers=jwt_token(2))
+    assert resp.status_code == 200
+    data = resp.data.decode('utf8').replace("'", '"')
+    data = json.loads(data)
+
+    assert data['club_section'] == ['조호원']
+    assert data['rooms'] == []
