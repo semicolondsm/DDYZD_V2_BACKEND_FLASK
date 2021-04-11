383e811106be6b89c959e46f1b9147cf9056f206
hwc9169 <hwc9169@gmail.com>
2021-04-11 15:05:28 +0900

    UPDATE; session_id to mobile, desktop session_id

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from app.models.type import RoomType
+from app.models.club import Room
 from app import db
 from flask_socketio import rooms
 
@@ -12,19 +13,19 @@ class User(db.Model):
     email = db.Column(db.String(50))
     device_token = db.Column(db.String(4096))
     bio = db.Column(db.String(256))
-    session_id = db.Column(db.String(256), nullable=True, default=False)
+    mobile_session_id = db.Column(db.String(255), default='0')
+    desktop_session_id = db.Column(db.String(255), default='0')
 
     rooms = db.relationship('Room', backref='user', lazy='dynamic')
     club_heads = db.relationship('ClubHead', backref='user')
     club_members = db.relationship('ClubMember', backref='user', lazy='dynamic')
 
-    from app.models.chat import Room
     def get_clubs(self):
         '''
         내가 동아리장인 동아리들 이름 출력하는 메서드
         '''
         clubs = []
-        from app.models.club import Club 
+        from app.models.club import Club
         for ch in self.club_heads:
             clubs.append(Club.query.get_or_404(ch.club_id)) 
         try:
@@ -51,13 +52,18 @@ class User(db.Model):
         내 방이 applicant 상태인지 아는 에서드
         '''
         room_query = Room.query.filter_by(user_id=self.id).filter_by(club_id=club.id)
+        from app import logger
+        logger.info("BES"+str(Room.query.all()[1].status))
         return room_query.filter(Room.status==RoomType.A.name).first()
 
     def is_scheduled(self, club):
         '''
         내 방이 scheduled 상태인지 아는 에서드
         '''
+        from app import logger
+        logger.info("BES: "+ str(Room.query.all()[1].status))
         room_query = Room.query.filter_by(user_id=self.id).filter_by(club_id=club.id)
+        logger.info(room_query.filter(Room.status==RoomType.S.name).first())
         return room_query.filter(Room.status==RoomType.S.name).first()
 
     def is_resulted(self, club):
@@ -76,6 +82,14 @@ class User(db.Model):
             return room_query.filter(Room.status!=RoomType.N.name).first() 
         return room_query.filter(Room.status!=RoomType.C.name).first()
 
+    def select_rooms(self):
+        rs = self.rooms.all()
+        rooms = []
+        for r in rs:
+            if r.chats.offset(r.u_offset).count() != 0:
+                rooms.append(r)
+                
+        return rooms
 
     def is_club_member(self, club):
         '''
@@ -94,8 +108,11 @@ class User(db.Model):
         return room.user_id == self.id
 
     def is_in_room(self, room):
-        room_list = rooms(sid=self.session_id, namespace='/chat')
-        return room.id in room_list
+        if room.id in rooms(sid=self.desktop_session_id, namespace='/chat'):
+            return True
+        if room.id in rooms(sid=self.mobile_session_id, namespace='/chat'):
+            return True
+        return False
 
     def __repr__(self):
         return '<User> {},{}'.format(self.name, self.gcn)
